cf6196c26eb789866b6c3ea5eb8d3a44509ad5f0
Sonja Peschke <sonjape91@gmail.com>
2023-01-09 11:50:19 +0100

    Adding a battle log

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
-      winner: null
+      winner: null,
+      // our logMessages array will contain several log messages that are objects
+      logMessages: []
     }
   },
   watch: {
@@ -55,6 +57,7 @@ const app = Vue.createApp({
       this.monsterHealth = 100;
       this.winner = null;
       this.currentRound = 0;
+      this.logMessages = [];
     },
     attackMonster() {
       // keeping track of the current round
@@ -64,17 +67,20 @@ const app = Vue.createApp({
       const attackValue = getRandomValue(5, 12);
       // reduce the monster's health by that damage
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue);
       // the monster should attack back whenever we attack it
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+      this.addLogMessage('monster', 'attack', attackValue);
     },
     specialAttackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'special-attack', attackValue);
       this.attackPlayer();
     },
     healPlayer() {
@@ -86,10 +92,19 @@ const app = Vue.createApp({
       } else {
         this.playerHealth += healValue;
       }
+      this.addLogMessage('player', 'heal', healValue);
       this.attackPlayer();
     },
     surrender() {
       this.winner = 'monster';
+    },
+    addLogMessage(who, what, value) {
+      // unshift is like push but it adds a new element at the beginning of an array
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value
+      });
     }
   }
 });
